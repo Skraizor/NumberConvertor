9b54501b053175cb22a91411c869562ccc3cdcf6
Tomáš Sukop <t.sukop@gmail.com>
2020-01-21 11:13:08 +0100

    Init commit

---
+++ NumberConvertor/Program.cs
@@ -0,0 +1,38 @@
+﻿using NumberConvertor.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NumberConvertor
+{
+	class Program
+	{
+		static void Main(string[] args) {
+			while (true) {
+				string number;
+				int numberBase;
+				int targetNumberBase;
+
+				Console.Write("Enter number: ");
+				number = Console.ReadLine().Trim();
+
+				Console.Write("Enter its base: ");
+				if(!int.TryParse(Console.ReadLine().Trim(), out numberBase) || numberBase < 2) {
+					Console.WriteLine("Base has to be whole number greater then 2");
+					continue;
+				}
+
+				Console.Write("Enter target base: ");
+				if (!int.TryParse(Console.ReadLine().Trim(), out targetNumberBase) || targetNumberBase < 2) {
+					Console.WriteLine("Base has to be whole number greater then 2");
+					continue;
+				}
+
+				string resultNumber = Convertor.Convert(number, numberBase, targetNumberBase);
+				Console.WriteLine(string.Format("Converted number {0} from base {1} to base {2} is {3}", number, numberBase, targetNumberBase, resultNumber));
+			}
+		}
+	}
+}
---
+++ NumberConvertor/Services/Convertor.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NumberConvertor.Services
+{
+	public class Convertor
+	{
+		/// <summary>
+		/// Converts number from numberBase to targetNumberBase via decimal base
+		/// </summary>
+		/// <param name="number"></param>
+		/// <param name="numberBase"></param>
+		/// <param name="targetNumberBase"></param>
+		/// <returns></returns>
+		public static string Convert(string number, int numberBase, int targetNumberBase) {
+			string result = number;
+			if (numberBase == targetNumberBase)
+				return result;
+			string decimalNumber = number;
+			if (numberBase != 10) {
+				decimalNumber = ConvertToDecimal(number, numberBase);
+			}
+			result = ConvertFromDecimal(decimalNumber, targetNumberBase);
+			return result;
+		}
+
+		/// <summary>
+		/// Converts number from any base to decimal
+		/// </summary>
+		/// <param name="number"></param>
+		/// <param name="numberBase"></param>
+		/// <returns></returns>
+		private static string ConvertToDecimal(string number, int numberBase) {
+			// check if number is negative
+			// check if number is whole number
+			// if it is non negative whole number convert vi regular method
+			int exponent = number.Length - 1;
+			return number;
+		}
+
+		private static string ConvertFromDecimal(string decimalNumber, int targetNumberBase) {
+
+		}
+	}
+}
